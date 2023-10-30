0fab92675a81023564d651cf16bc1f8ece2c84c0
Kento Ueda <beatweapon7th@gmail.com>
2023-10-31 03:50:58 +0900

    通知を求めるコンポーネントを差し込み

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script>
+	import NotificationPermission from 'src/lib/components/NotificationPermission.svelte';
 	export let data;
 
 	const organizations = data.currentUser?.organizations || [];
 </script>
 
 <h1>Welcome to ThanksCard</h1>
+
+{#if data.currentUser?.uid}
+	<NotificationPermission uid={data.currentUser.uid} />
+{/if}
 
 {#each organizations as organization}
 	<a href="/{organization}">{organization}</a>
