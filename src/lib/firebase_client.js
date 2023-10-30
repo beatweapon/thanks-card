2e89cb5510a56a523fceb52ef39b38337a743384
Kento Ueda <beatweapon7th@gmail.com>
2023-10-31 03:50:22 +0900

    firebaseの初期化処理に変数を追加

--- src/lib/firebase_client.js
+++ src/lib/firebase_client.js
@@ -1,4 +1,10 @@
-import { PUBLIC_API_KEY, PUBLIC_AUTH_DOMAIN, PUBLIC_PROJECT_ID } from '$env/static/public';
+import {
+	PUBLIC_API_KEY,
+	PUBLIC_AUTH_DOMAIN,
+	PUBLIC_PROJECT_ID,
+	PUBLIC_MESSAGING_SENDER_ID,
+	PUBLIC_APP_ID,
+} from '$env/static/public';
 import { initializeApp } from 'firebase/app';
 import { getAuth, setPersistence, inMemoryPersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
@@ -6,7 +12,9 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
 	apiKey: PUBLIC_API_KEY,
 	authDomain: PUBLIC_AUTH_DOMAIN,
-	projectId: PUBLIC_PROJECT_ID
+	projectId: PUBLIC_PROJECT_ID,
+	appId: PUBLIC_APP_ID,
+	messagingSenderId: PUBLIC_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
