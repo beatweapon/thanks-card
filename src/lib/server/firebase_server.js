0edd021ac42810b01eef0a26cae84b15fa35b8a8
Kento Ueda <beatweapon7th@gmail.com>
2023-11-03 22:23:18 +0900

    通知送信処理を実装

--- src/lib/server/firebase_server.js
+++ src/lib/server/firebase_server.js
@@ -2,3 +2,31 @@ import admin from 'firebase-admin';
 
 const app = admin.initializeApp();
 export const auth = app.auth();
+
+/**
+ * Push通知送信処理
+ * @param {string} registrationToken
+ * @param {string} title
+ * @param {string} body
+ * @param {string} icon
+ */
+export const sendMessageToDevice = (registrationToken, title, body, icon) => {
+	admin
+		.messaging()
+		.send({
+			token: registrationToken,
+			notification: {
+				title,
+				body,
+			},
+			data: {
+				icon,
+			},
+		})
+		.then((response) => {
+			console.log('Push通知が送信されました:', response);
+		})
+		.catch((error) => {
+			console.error('Push通知の送信に失敗しました:', error);
+		});
+};
