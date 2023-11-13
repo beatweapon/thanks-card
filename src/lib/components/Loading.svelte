69867ad2ab95130bff5eed8d52b84346257684a4
Kento Ueda <beatweapon7th@gmail.com>
2023-11-14 08:09:44 +0900

    サインイン時のローディングを実装

---
+++ src/lib/components/Loading.svelte
@@ -0,0 +1,67 @@
+<div class="loading">
+	<div class="dot" />
+	<div class="dot" />
+	<div class="dot" />
+	<div class="dot" />
+	<div class="dot" />
+</div>
+
+<style>
+	.loading {
+		display: flex;
+	}
+
+	.dot {
+		width: 2rem;
+		height: 2rem;
+		margin: 0.8rem;
+		border-radius: 50%;
+	}
+
+	.dot:nth-child(1) {
+		background: hsl(calc(45 * 0) 100% 75%);
+		animation: wave 2s ease-out infinite;
+		animation-delay: calc(0.2s * 0);
+	}
+
+	.dot:nth-child(2) {
+		background: hsl(calc(45 * 1) 100% 75%);
+		animation: wave 2s ease-out infinite;
+		animation-delay: calc(0.2s * 1);
+	}
+
+	.dot:nth-child(3) {
+		background: hsl(calc(45 * 2) 100% 75%);
+		animation: wave 2s ease-out infinite;
+		animation-delay: calc(0.2s * 2);
+	}
+
+	.dot:nth-child(4) {
+		background: hsl(calc(45 * 3) 100% 75%);
+		animation: wave 2s ease-out infinite;
+		animation-delay: calc(0.2s * 3);
+	}
+
+	.dot:nth-child(5) {
+		background: hsl(calc(45 * 4) 100% 75%);
+		animation: wave 2s ease-out infinite;
+		animation-delay: calc(0.2s * 4);
+	}
+
+	@keyframes wave {
+		0% {
+			opacity: 0;
+			transform: translateY(-5rem);
+		}
+
+		50%,
+		75% {
+			transform: translateY(0);
+		}
+
+		80%,
+		100% {
+			opacity: 1;
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,17 @@
 <script>
 	import './global.css';
 	import auth from '$lib/auth';
+	import Loading from 'src/lib/components/Loading.svelte';
 	export let data;
 
+	let isProcessing = false;
+
 	const signIn = async () => {
-		await auth.signIn();
+		isProcessing = true;
+
+		await auth.signIn().finally(() => {
+			isProcessing = false;
+		});
 		location.reload();
 	};
 
@@ -14,9 +21,25 @@
 	};
 </script>
 
-{#if data.currentUser}
+{#if isProcessing}
+	<div class="center">
+		<Loading />
+	</div>
+{:else if data.currentUser}
 	<slot />
-	<!-- <button on:click={signOut}> Logout </button> -->
+	<button on:click={signOut}> Logout </button>
 {:else}
-	<button on:click={signIn}> Login using Google </button>
+	<div class="center">
+		<button on:click={signIn}> Login using Google </button>
+	</div>
 {/if}
+
+<style>
+	.center {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100dvh;
+		width: 100%;
+	}
+</style>
