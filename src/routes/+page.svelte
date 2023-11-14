a825c6016b2e6f9151c052775747151018a286e6
Kento Ueda <beatweapon7th@gmail.com>
2023-11-14 09:17:52 +0900

    所属組織が一つの場合は組織のトップ画面にリダイレクトする

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script>
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	export let data;
 
 	const organizations = data.currentUser?.organizations || [];
+
+	if (organizations.length === 1) {
+		goto(`${$page.url}/${organizations[0]}`);
+	}
 </script>
 
 <h1>Welcome to ThanksCard</h1>
