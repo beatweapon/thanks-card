0f2f99370376dcb6e054021145744f32987a126d
Kento Ueda <beatweapon7th@gmail.com>
2023-11-15 06:39:16 +0900

    gotoはサーバーのレンダリング時に使えないので修正

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,16 @@
 <script>
+	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	export let data;
 
 	const organizations = data.currentUser?.organizations || [];
 
-	if (organizations.length === 1) {
-		goto(`${$page.url}/${organizations[0]}`);
-	}
+	onMount(() => {
+		if (organizations.length === 1) {
+			goto(`${$page.url}/${organizations[0]}`);
+		}
+	});
 </script>
 
 <h1>Welcome to ThanksCard</h1>
