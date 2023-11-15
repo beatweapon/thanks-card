23f86cb89bc823b9c7f2f233dc687a3a9f887e61
Kento Ueda <beatweapon7th@gmail.com>
2023-11-15 09:05:43 +0900

    トップページのデザインを更新

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,8 +13,43 @@
 	});
 </script>
 
-<h1>Welcome to ThanksCard</h1>
+<main class="main">
+	<h1 class="title">Welcome to ThanksCard</h1>
 
-{#each organizations as organization}
-	<a href="/{organization}">{organization}</a>
-{/each}
+	<h2 class="sub_title">あなたの参加中組織一覧</h2>
+	<ul>
+		{#each organizations as organization}
+			<li>
+				<a href="/{organization}">{organization}</a>
+			</li>
+		{/each}
+	</ul>
+</main>
+
+<style>
+	.main {
+		height: 100dvh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.title {
+		--color1: #edb658;
+		--color2: #e7719b;
+		background: linear-gradient(45deg, var(--color1), var(--color2));
+		-webkit-background-clip: text;
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+
+	.sub_title {
+		--color1: #0072b4;
+		--color2: #7bc0be;
+		background: linear-gradient(45deg, var(--color1), var(--color2));
+		-webkit-background-clip: text;
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+</style>
