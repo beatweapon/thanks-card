b0bc445d657c7eae86866fc01814bdce5b289c7a
Kento Ueda <beatweapon7th@gmail.com>
2023-11-17 09:28:51 +0900

    ログインユーザーの型を定義

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,16 +1,12 @@
+import { User } from 'src/types/user';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			currentUser:
-				| {
-						uid: string;
-						email?: string;
-						picture?: string;
-				  }
-				| undefined;
+			currentUser: User | undefined;
 		}
 		// interface PageData {}
 		// interface Platform {}
