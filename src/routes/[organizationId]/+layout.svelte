7abfd71a4acbbc7b941b64826eac3e9f78be9e5c
Kento Ueda <beatweapon7th@gmail.com>
2023-11-20 05:44:46 +0900

    通知設定をレイアウトからページに移動

--- src/routes/[organizationId]/+layout.svelte
+++ src/routes/[organizationId]/+layout.svelte
@@ -1,53 +1,17 @@
 <script>
-	import { onMount } from 'svelte';
 	import Header from 'src/lib/components/Header.svelte';
-	import NotificationPermission from '$lib/components/NotificationPermission.svelte';
-	import PlainButton from 'src/lib/components/design/PlainButton.svelte';
-	import Loading from 'src/lib/components/Loading.svelte';
 
 	export let data;
-
-	// 通知の許可ステータスを追跡するための変数
-	let permissionStatus = '';
-
-	onMount(async () => {
-		permissionStatus = Notification.permission;
-	});
 </script>
 
 <Header organizationName={data.organization?.name} />
 
-{#if permissionStatus === ''}
-	<div class="center">
-		<Loading />
-	</div>
-{:else if permissionStatus === 'default'}
-	<div class="center">
-		<div>
-			<p>
-				あなたにカードが届いたときや、あなたが贈ったカードにリアクションがついたときに通知を受け取ることができます。
-			</p>
-			<NotificationPermission uid={data.currentUser.uid} />
-
-			<PlainButton on:click={() => (permissionStatus = 'skip')}>スキップ</PlainButton>
-		</div>
-	</div>
-{:else}
-	<main class="main">
-		<slot />
-	</main>
-{/if}
+<main class="main">
+	<slot />
+</main>
 
 <style>
 	.main {
-		margin: 0.5rem;
-	}
-
-	.center {
-		margin: 1rem;
-		height: calc(100dvh - 40px);
-		display: grid;
-		justify-content: center;
-		align-items: center;
+		margin: 0 0.5rem;
 	}
 </style>
--- src/routes/[organizationId]/+page.svelte
+++ src/routes/[organizationId]/+page.svelte
@@ -1,8 +1,11 @@
 <script>
+	import { onMount } from 'svelte';
 	import { base } from '$app/paths';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { members, watchMemberCollection } from '$lib/stores/members.js';
+	import Loading from 'src/lib/components/Loading.svelte';
+	import PlainButton from 'src/lib/components/design/PlainButton.svelte';
 	import NotificationPermission from '$lib/components/NotificationPermission.svelte';
 	import Qr from '$lib/components/Qr.svelte';
 	import In7DaysCards from 'src/lib/components/views/[organizationId]/In7DaysCards.svelte';
@@ -112,53 +115,87 @@
 			}
 		);
 	};
+
+	// 通知の許可ステータスを追跡するための変数
+	let permissionStatus = '';
+
+	onMount(async () => {
+		permissionStatus = Notification.permission;
+	});
 </script>
 
-<h2>Welcome to TopPage</h2>
-<button on:click={() => goto(`${$page.url}/thanksCardEditor`)}>カードを送る</button>
-<a href={`${$page.url}/profile`}>名前を変更する</a>
+{#if permissionStatus === ''}
+	<div class="center">
+		<Loading />
+	</div>
+{:else if permissionStatus === 'default'}
+	<div class="center">
+		<div>
+			<p>
+				あなたにカードが届いたときや、あなたが贈ったカードにリアクションがついたときに通知を受け取ることができます。
+			</p>
+			<NotificationPermission uid={data.currentUser.uid} />
 
-<button on:click={toggleShowCardList}>
-	{#if isShowAllCardList}
-		直近7日
-	{:else}
-		全て
-	{/if}
-</button>
-
-{#if isShowAllCardList}
-	<AllCards
-		organizationId={$page.params.organizationId}
-		currentUser={data.currentUser}
-		members={$members}
-		{cardFilter}
-		{cardDeletingSlot}
-		on:clickUser={(e) => setFilterOptionFrom(e.detail.uid)}
-		on:clickFrom={(e) => setFilterOptionFrom(e.detail)}
-		on:clickTo={(e) => setFilterOptionTo(e.detail)}
-		on:addReaction={(e) => addReaction(e.detail.card, e.detail.emoji)}
-		on:addCardDeletingSlot={(e) => addCardDeletingSlot(e.detail)}
-		on:removeCardDeletingSlot={(e) => removeCardDeletingSlot(e.detail)}
-	/>
+			<PlainButton on:click={() => (permissionStatus = 'skip')}>スキップ</PlainButton>
+		</div>
+	</div>
 {:else}
-	<In7DaysCards
-		organizationId={$page.params.organizationId}
-		currentUser={data.currentUser}
-		members={$members}
-		{cardFilter}
-		{cardDeletingSlot}
-		on:clickUser={(e) => setFilterOptionFrom(e.detail.uid)}
-		on:clickFrom={(e) => setFilterOptionFrom(e.detail)}
-		on:clickTo={(e) => setFilterOptionTo(e.detail)}
-		on:addReaction={(e) => addReaction(e.detail.card, e.detail.emoji)}
-		on:addCardDeletingSlot={(e) => addCardDeletingSlot(e.detail)}
-		on:removeCardDeletingSlot={(e) => removeCardDeletingSlot(e.detail)}
-	/>
+	<h2>Welcome to TopPage</h2>
+	<button on:click={() => goto(`${$page.url}/thanksCardEditor`)}>カードを送る</button>
+	<a href={`${$page.url}/profile`}>名前を変更する</a>
+
+	<button on:click={toggleShowCardList}>
+		{#if isShowAllCardList}
+			直近7日
+		{:else}
+			全て
+		{/if}
+	</button>
+
+	{#if isShowAllCardList}
+		<AllCards
+			organizationId={$page.params.organizationId}
+			currentUser={data.currentUser}
+			members={$members}
+			{cardFilter}
+			{cardDeletingSlot}
+			on:clickUser={(e) => setFilterOptionFrom(e.detail.uid)}
+			on:clickFrom={(e) => setFilterOptionFrom(e.detail)}
+			on:clickTo={(e) => setFilterOptionTo(e.detail)}
+			on:addReaction={(e) => addReaction(e.detail.card, e.detail.emoji)}
+			on:addCardDeletingSlot={(e) => addCardDeletingSlot(e.detail)}
+			on:removeCardDeletingSlot={(e) => removeCardDeletingSlot(e.detail)}
+		/>
+	{:else}
+		<In7DaysCards
+			organizationId={$page.params.organizationId}
+			currentUser={data.currentUser}
+			members={$members}
+			{cardFilter}
+			{cardDeletingSlot}
+			on:clickUser={(e) => setFilterOptionFrom(e.detail.uid)}
+			on:clickFrom={(e) => setFilterOptionFrom(e.detail)}
+			on:clickTo={(e) => setFilterOptionTo(e.detail)}
+			on:addReaction={(e) => addReaction(e.detail.card, e.detail.emoji)}
+			on:addCardDeletingSlot={(e) => addCardDeletingSlot(e.detail)}
+			on:removeCardDeletingSlot={(e) => removeCardDeletingSlot(e.detail)}
+		/>
+	{/if}
+
+	<h2>この画面のQRコード</h2>
+	<Qr url={$page.url.href} />
+
+	{#if data.currentUser?.uid}
+		<NotificationPermission uid={data.currentUser.uid} />
+	{/if}
 {/if}
 
-<h2>この画面のQRコード</h2>
-<Qr url={$page.url.href} />
-
-{#if data.currentUser?.uid}
-	<NotificationPermission uid={data.currentUser.uid} />
-{/if}
+<style>
+	.center {
+		margin: 0 1rem;
+		height: calc(100dvh - 2rem);
+		display: grid;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
