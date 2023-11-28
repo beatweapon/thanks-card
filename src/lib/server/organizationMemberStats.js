95c4ef6256e1e9c6db7ad3923864aa5357352840
Kento Ueda <beatweapon7th@gmail.com>
2023-11-28 09:37:14 +0900

    統計情報の取得、更新処理を実装

---
+++ src/lib/server/organizationMemberStats.js
@@ -0,0 +1,28 @@
+import { getFirestore } from 'firebase-admin/firestore';
+
+/**
+ * 組織メンバーの統計を取得する
+ * @param {string} organizationId
+ * @param {string} uid
+ */
+export const fetchOrganizationMemberStats = async (organizationId, uid) => {
+	const db = getFirestore();
+	const docRef = db.doc(`organizations/${organizationId}/membersStats/${uid}`);
+	const docSnap = await docRef.get();
+
+	if (docSnap.exists) {
+		return /** @type {import('src/types/organization/memberStats').Stats} */ (docSnap.data());
+	}
+};
+
+/**
+ * 組織メンバーの統計を更新する
+ * @param {string} organizationId
+ * @param {string} uid
+ * @param {import('src/types/organization/memberStats').UpdateData} data
+ */
+export const updateOrganizationMemberStats = async (organizationId, uid, data) => {
+	const db = getFirestore();
+	const docRef = db.doc(`organizations/${organizationId}/membersStats/${uid}`);
+	return await docRef.update(data);
+};
