beb7b2111025c01d266b91c101b3e5d2d9ffab15
Kento Ueda <beatweapon7th@gmail.com>
2023-12-04 09:59:29 +0900

    実績のデータを作成

---
+++ src/lib/data/achievements.js
@@ -0,0 +1,26 @@
+export const ahiecements = {
+	sendThanks: {
+		title: '感謝の気持ち',
+		description: 'サンクスカードを贈る',
+	},
+	sendReaction: {
+		title: 'ありがとうへのありがとう',
+		description: 'カードにリアクションする',
+	},
+	sendThanksToMe: {
+		title: '自分を褒める',
+		description: 'サンクスカードを自分に贈る',
+	},
+	deleteThanks: {
+		title: '間違えちゃった',
+		description: 'サンクスカードを削除する',
+	},
+	updateProfile: {
+		title: '私こういう者です',
+		description: 'プロフィールを更新する',
+	},
+	notification: {
+		title: 'カードまだかな？',
+		description: '通知設定を許可する',
+	},
+};
