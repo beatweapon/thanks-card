1f53afc45f735e3827ec4f826f9406b873e07fbd
Kento Ueda <beatweapon7th@gmail.com>
2023-12-05 04:12:55 +0900

    実績の監視処理を実装

---
+++ src/lib/stores/membersAchievement.js
@@ -0,0 +1,31 @@
+import { writable } from 'svelte/store';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from 'src/lib/firebase_client';
+
+/**
+ * 実績
+ * @type {import('svelte/store').Writable<import('src/types/organization/memberAchievement').Ahiecements>}
+ */
+export const achievement = writable({});
+
+/** @type {import("firebase/firestore").Unsubscribe | undefined} */
+let unsubscribe;
+
+/**
+ * 実績を監視、更新する
+ * @param {string} organizationId
+ * @param {string} uid
+ */
+export const watchMemberAchievementCollection = (organizationId, uid) => {
+	if (unsubscribe) {
+		return;
+	}
+
+	const docRef = doc(db, `organizations/${organizationId}/membersAchievement/${uid}`);
+
+	unsubscribe = onSnapshot(docRef, (doc) => {
+		achievement.set(
+			/** @type {import('src/types/organization/memberAchievement').Ahiecements} */ (doc.data())
+		);
+	});
+};
