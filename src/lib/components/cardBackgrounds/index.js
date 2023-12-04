b0aa1123fd4942cacd8894afb054a4aaf1be2a21
Kento Ueda <beatweapon7th@gmail.com>
2023-12-05 05:08:01 +0900

    カードデザイン6を実装

--- src/lib/components/cardBackgrounds/index.js
+++ src/lib/components/cardBackgrounds/index.js
@@ -4,5 +4,6 @@ import card_2 from './2.svelte';
 import card_3 from './3.svelte';
 import card_4 from './4.svelte';
 import card_5 from './5.svelte';
+import card_6 from './6.svelte';
 
-export { card_0, card_1, card_2, card_3, card_4, card_5 };
+export { card_0, card_1, card_2, card_3, card_4, card_5, card_6 };
