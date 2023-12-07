4bcc43a3e9e4a9fb9bc357f021af7ce791fe16e7
Kento Ueda <beatweapon7th@gmail.com>
2023-12-08 05:39:39 +0900

    Chrome用にインストール機能を実装

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { onMount } from 'svelte';
 	import './global.css';
 	import auth from '$lib/auth';
 	import Loading from 'src/lib/components/Loading.svelte';
@@ -20,6 +21,46 @@
 		await auth.signOut();
 		location.reload();
 	};
+
+	/**
+	 * @type {any|null} deferredPrompt - インストールプロンプトの遅延オブジェクト
+	 */
+	let deferredPrompt;
+
+	onMount(() => {
+		/**
+		 * beforeinstallprompt イベントのリスナー
+		 * @param {any} e - beforeinstallprompt イベントオブジェクト
+		 */
+		window.addEventListener('beforeinstallprompt', (e) => {
+			console.log('beforeinstallprompt');
+			// インストールのポップアップを保持
+			deferredPrompt = e;
+
+			// ポップアップをキャンセルして自動的には表示しない
+			e.preventDefault();
+		});
+	});
+
+	// インストールボタンなどのユーザーアクションがあった時にポップアップを表示
+	const install = () => {
+		if (deferredPrompt) {
+			// インストールポップアップを表示
+			deferredPrompt.prompt();
+
+			// ポップアップが終了したら、結果を検証
+			deferredPrompt.userChoice.then((choiceResult) => {
+				if (choiceResult.outcome === 'accepted') {
+					console.log('ユーザーがインストールを選択');
+				} else {
+					console.log('ユーザーがインストールをキャンセル');
+				}
+
+				// ポップアップの情報をリセット
+				deferredPrompt = null;
+			});
+		}
+	};
 </script>
 
 {#if isProcessing}
@@ -29,6 +70,7 @@
 {:else if data.currentUser}
 	<slot />
 	<!-- <button on:click={signOut}> Logout </button> -->
+	<button on:click={install}> インストール </button>
 {:else}
 	<div class="center">
 		<button on:click={signIn}> Login using Google </button>
