f305bddcb4a62038806d6110d0b6a197eda7a1b9
Kento Ueda <beatweapon7th@gmail.com>
2024-01-19 09:14:57 +0900

    フレーム購入APIを実装

---
+++ src/routes/api/organizations/[organizationId]/shop/+server.js
@@ -0,0 +1,20 @@
+import { FieldValue } from 'firebase-admin/firestore';
+import { auth } from '$lib/server/firebase_server';
+import { updateOrganizationMemberStats } from '$lib/server/organizationMemberStats';
+
+export const PUT = async ({ request, params, cookies }) => {
+	const session = cookies.get('__session') || '';
+	if (session) {
+		const decodedClaims = await auth.verifySessionCookie(session);
+		const { uid } = decodedClaims;
+		const { organizationId } = params;
+		const { frame, price } = await request.json();
+
+		await updateOrganizationMemberStats(organizationId, uid, {
+			point: FieldValue.increment(-price),
+			'item.frames': FieldValue.arrayUnion(frame),
+		});
+	}
+
+	return new Response();
+};
