676b387ef3bb88ff604bf893a99b01874f95a4ad
Kento Ueda <beatweapon7th@gmail.com>
2024-01-31 09:57:44 +0900

    メンバーグループ一覧画面を実装

---
+++ src/routes/[organizationId]/memberGroup/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { page } from '$app/stores';
+  import { goto } from '$app/navigation';
+  import { memberGroups, watchMemberGroupCollection } from '$lib/stores/memberGroup.js';
+
+  watchMemberGroupCollection($page.params.organizationId);
+
+  /**
+   * 新規作成画面へ遷移する
+   */
+  const goToCreate = () => {
+    goto(`${$page.url}/create`);
+  };
+
+  /**
+   * 編集画面へ遷移する
+   * @param {string} id
+   */
+  const goToEdit = (id) => {
+    goto(`${$page.url}/${id}`);
+  };
+</script>
+
+<h2>メンバーグループ</h2>
+
+<button on:click={() => goToCreate()}>新規作成</button>
+
+{#each $memberGroups as group}
+  <button on:click={() => goToEdit(group.id)}>{group.name} ({group.memberIds.length}人)</button>
+{/each}
