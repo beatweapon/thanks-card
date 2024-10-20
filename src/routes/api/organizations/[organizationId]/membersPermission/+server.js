b6f115f4ca6941614ace8476d472cdd60d2ad825
Kento Ueda <beatweapon7th@gmail.com>
2024-10-21 04:13:03 +0900

    更新処理に権限判定処理を追加

--- src/routes/api/organizations/[organizationId]/membersPermission/+server.js
+++ src/routes/api/organizations/[organizationId]/membersPermission/+server.js
@@ -1,9 +1,28 @@
+import { auth } from '$lib/server/firebase_server';
 import { getFirestore } from 'firebase-admin/firestore';
 
-export const PUT = async ({ request, params }) => {
-  const db = getFirestore();
+export const PUT = async ({ request, params, cookies }) => {
+  const session = cookies.get('__session') || '';
+  if (!session) {
+    return new Response('Forbidden', { status: 403 });
+  }
 
+  const decodedClaims = await auth.verifySessionCookie(session);
+  const { uid } = decodedClaims;
   const { organizationId } = params;
+
+  const db = getFirestore();
+  const docRef = db.collection(`organizations/${organizationId}/members`).doc(uid);
+  const docSnap = await docRef.get();
+
+  if (!docSnap.exists) {
+    return new Response('Forbidden', { status: 403 });
+  }
+
+  if (!docSnap.data()?.permission.admin) {
+    return new Response('Forbidden', { status: 403 });
+  }
+
   const { members } = await request.json();
 
   const batch = db.batch(); // バッチ処理を使用して一括更新
