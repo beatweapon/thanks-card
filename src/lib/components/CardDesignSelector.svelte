64902b26582fb7ae585f51d8a29e31c601d49bf7
Kota Kameyama <smoking.cat2392@gmail.com>
2024-12-12 12:15:11 +0900

    7枚目のカードデザインを追加

--- src/lib/components/CardDesignSelector.svelte
+++ src/lib/components/CardDesignSelector.svelte
@@ -6,7 +6,7 @@
   /** @type {string} */
   export let selectedDesignId = '0';
 
-  const designIds = ['0', '1', '2', '3', '4', '5', '6'];
+  const designIds = ['0', '1', '2', '3', '4', '5', '6', '7'];
 
   /**
    * @type {Array<{
