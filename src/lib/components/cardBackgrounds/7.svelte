e35ac49389f0c868a0acad8c1eb04565534d8dd6
KotaKameyama <45307875+KotaKameyama@users.noreply.github.com>
2024-12-12 12:39:02 +0900

    7枚目のカードデザインを追加 (#1)

---
+++ src/lib/components/cardBackgrounds/7.svelte
@@ -0,0 +1,19 @@
+<div class="card_back">
+  <div class="card_back_cover">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .card_back {
+    color: black;
+    font-weight: bold;
+    background-image: url('/images/card/japanese_cats.jpg');
+    background-size: cover;
+    background-position: center -20px;
+  }
+
+  .card_back_cover {
+    background-color: rgba(255, 255, 255, 0.85);
+  }
+</style>
