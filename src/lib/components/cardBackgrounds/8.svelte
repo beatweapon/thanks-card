55f0335a0cd27eefe6450e29ad27f746013ca956
Kota Kameyama <smoking.cat2392@gmail.com>
2025-01-11 12:13:51 +0900

    ピンクの花のカードデザインを追加

---
+++ src/lib/components/cardBackgrounds/8.svelte
@@ -0,0 +1,21 @@
+<div class="card_back">
+  <div class="card_back_cover">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .card_back {
+    color: #772057;
+    font-weight: bold;
+    background: #ffeff7 right 0% no-repeat url('/images/card/8/flower_series_rose.svg');
+    background-size: 30%;
+    transform: rotate(180deg);
+  }
+
+  .card_back_cover {
+    background: right 0% no-repeat url('/images/card/8/flower_series_rose.svg');
+    background-size: 30%;
+    transform: rotate(180deg);
+  }
+</style>
--- src/lib/components/cardBackgrounds/index.js
+++ src/lib/components/cardBackgrounds/index.js
@@ -6,5 +6,6 @@ import card_4 from './4.svelte';
 import card_5 from './5.svelte';
 import card_6 from './6.svelte';
 import card_7 from './7.svelte';
+import card_8 from './8.svelte';
 
-export { card_0, card_1, card_2, card_3, card_4, card_5, card_6, card_7 };
+export { card_0, card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8 };
