902942b55772bc88335c622406b73bdacf90fad6
KotaKameyama <45307875+KotaKameyama@users.noreply.github.com>
2025-01-14 05:58:25 +0900

    ピンクの花のカードデザインを追加 (#2)

--- src/lib/components/CardDesignSelector.svelte
+++ src/lib/components/CardDesignSelector.svelte
@@ -6,7 +6,7 @@
   /** @type {string} */
   export let selectedDesignId = '0';
 
-  const designIds = ['0', '1', '2', '3', '4', '5', '6', '7'];
+  const designIds = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
 
   /**
    * @type {Array<{
