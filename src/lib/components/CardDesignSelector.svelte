29f8a1c280a29cead1481abb992599e5273e7125
Kota Kameyama <smoking.cat2392@gmail.com>
2025-01-30 12:23:12 +0900

    カードデザインのIDを修正

--- src/lib/components/CardDesignSelector.svelte
+++ src/lib/components/CardDesignSelector.svelte
@@ -6,7 +6,7 @@
   /** @type {string} */
   export let selectedDesignId = '0';
 
-  const designIds = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  const designIds = ['0', '1', '2', '3', '4', '5', '6', '7', '13', '14'];
 
   /**
    * @type {Array<{
--- src/lib/components/cardBackgrounds/index.js
+++ src/lib/components/cardBackgrounds/index.js
@@ -6,8 +6,7 @@ import card_4 from './4.svelte';
 import card_5 from './5.svelte';
 import card_6 from './6.svelte';
 import card_7 from './7.svelte';
-import card_8 from './8.svelte';
-import card_9 from './9.svelte';
-import card_10 from './10.svelte';
+import card_13 from './13.svelte';
+import card_14 from './14.svelte';
 
-export { card_0, card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8, card_9, card_10 };
+export { card_0, card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_13, card_14 };
